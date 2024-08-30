3b8532b9b01a15ce70ad1a42f349e9f531994215
faizsr <fazzfaiz123@gmail.com>
2024-08-30 20:10:36 +0530

    String extensions moved to folder

--- lib/src/config/constants/strings.dart
+++ lib/src/config/constants/strings.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 const String montserrat = 'Montserrat';
 
 List<String> brands = [
@@ -15,3 +17,10 @@ List<String> brands = [
   'VOLKSWAGEN',
   'ASTON MARTIN'
 ];
+
+extension HexToColor on String {
+  Color hexToColor() {
+    return Color(
+        int.parse(toLowerCase().substring(1, 7), radix: 16) + 0xFF000000);
+  }
+}
--- lib/src/feature/rental/presentation/views/car_detailed_page.dart
+++ lib/src/feature/rental/presentation/views/car_detailed_page.dart
@@ -238,9 +238,4 @@ class CarDetailedPage extends StatelessWidget {
   }
 }
 
-extension HexToColor on String {
-  Color hexToColor() {
-    return Color(
-        int.parse(toLowerCase().substring(1, 7), radix: 16) + 0xFF000000);
-  }
-}
+
